21c5f1047bf200303e1c9a8bedacbf7cf1c16864
Karol Kornecki <karol.kornecki@gmail.com>
2015-11-29 18:53:36 +0100

    #120 Triangle

---
+++ src/main/java/pl/ksolutions/leetcode/Triangle.java
@@ -0,0 +1,41 @@
+package pl.ksolutions.leetcode;
+
+import java.util.List;
+
+/**
+ * Given a triangle, find the minimum path sum from top to bottom. Each step you may move to adjacent numbers on the row below.
+ * <p/>
+ * For example, given the following triangle
+ * <p/>
+ * [
+ * [2],
+ * [3,4],
+ * [6,5,7],
+ * [4,1,8,3]
+ * ]
+ * <p/>
+ * The minimum path sum from top to bottom is 11 (i.e., 2 + 3 + 5 + 1 = 11).
+ * <p/>
+ * Note:
+ * Bonus point if you are able to do this using only O(n) extra space, where n is the total number of rows in the triangle.
+ *
+ * @author Karol Kornecki
+ */
+public class Triangle {
+
+    public int minimumTotal(List<List<Integer>> triangle) {
+        if (triangle == null || triangle.isEmpty()) {
+            return 0;
+        }
+
+        for (int i = triangle.size() - 2; i >= 0; --i) {
+            for (int j = 0; j < triangle.get(i).size(); ++j) {
+                int value = triangle.get(i).get(j) + Math.min(triangle.get(i + 1).get(j), triangle.get(i + 1).get(j + 1));
+                triangle.get(i).add(j, value);
+                triangle.get(i).remove(j + 1);
+
+            }
+        }
+        return triangle.get(0).get(0);
+    }
+}
