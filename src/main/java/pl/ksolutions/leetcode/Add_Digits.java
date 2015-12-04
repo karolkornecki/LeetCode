cd09e96024507c572626927efcd67be3dd1f7f7f
Karol Kornecki <karol.kornecki@gmail.com>
2015-12-04 20:24:49 +0100

    #258 Add Digits - defect

--- src/main/java/pl/ksolutions/leetcode/Add_Digits.java
+++ src/main/java/pl/ksolutions/leetcode/Add_Digits.java
@@ -39,6 +39,9 @@ public class Add_Digits {
         if (num <= 9) {
             return num;
         }
+        if (num % 9 == 0) {
+            return 9;
+        }
         return num % 9;
     }
 }
