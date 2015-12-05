767aa91653940874044ba9ef15bd9b21783dc976
Karol Kornecki <karol.kornecki@gmail.com>
2015-12-05 22:14:56 +0100

    #83 Remove Duplicates from Sorted List

---
+++ src/main/java/pl/ksolutions/leetcode/easy/list/Remove_Duplicates_from_Sorted_List.java
@@ -0,0 +1,41 @@
+package pl.ksolutions.leetcode.easy.list;
+
+import org.junit.Assert;
+import pl.ksolutions.leetcode.structures.ListNode;
+
+/**
+ * Difficulty: Easy
+ * <p>
+ * Given a sorted linked list, delete all duplicates such that each element appear only once.
+ * <p>
+ * For example,
+ * Given 1->1->2, return 1->2.
+ * Given 1->1->2->3->3, return 1->2->3.
+ *
+ * @author Karol Kornecki
+ */
+public class Remove_Duplicates_from_Sorted_List {
+
+    public static void main(String[] args) {
+        Assert.assertEquals("321", new Remove_Duplicates_from_Sorted_List().deleteDuplicates(new ListNode(3, new ListNode(3, new ListNode(3, new ListNode(2, new ListNode(2, new ListNode(1, new ListNode(1, null)))))))).toString());
+        Assert.assertEquals("3210", new Remove_Duplicates_from_Sorted_List().deleteDuplicates(new ListNode(3, new ListNode(3, new ListNode(3, new ListNode(2, new ListNode(2, new ListNode(1, new ListNode(1, new ListNode(0, null))))))))).toString());
+
+    }
+
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode current = head;
+        ListNode node = current;
+        while (current != null) {
+            if (current.val != node.val) {
+                node.next = current;
+                node = node.next;
+            }
+            current = current.next;
+        }
+        node.next = null;
+        return head;
+    }
+}
