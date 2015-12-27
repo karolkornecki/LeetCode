575a03d7c18b3f96eb7114f230ebf3b755c6d36d
Karol Kornecki <karol.kornecki@gmail.com>
2015-12-27 16:49:18 +0100

    #184 Department Highest Salary

---
+++ src/main/java/pl/ksolutions/leetcode/database/medium/Department_Highest_Salary.sql
@@ -0,0 +1,35 @@
+-- Difficulty: Medium
+--
+-- The Employee table holds all employees. Every employee has an Id, a salary, and there is also a column for the department Id.
+--
+-- +----+-------+--------+--------------+
+-- | Id | Name  | Salary | DepartmentId |
+-- +----+-------+--------+--------------+
+-- | 1  | Joe   | 70000  | 1            |
+-- | 2  | Henry | 80000  | 2            |
+-- | 3  | Sam   | 60000  | 2            |
+-- | 4  | Max   | 90000  | 1            |
+-- +----+-------+--------+--------------+
+--
+-- The Department table holds all departments of the company.
+--
+-- +----+----------+
+-- | Id | Name     |
+-- +----+----------+
+-- | 1  | IT       |
+-- | 2  | Sales    |
+-- +----+----------+
+--
+-- Write a SQL query to find employees who have the highest salary in each of the departments. For the above tables, Max has the highest salary in the IT department and Henry has the highest salary in the Sales department.
+--
+-- +------------+----------+--------+
+-- | Department | Employee | Salary |
+-- +------------+----------+--------+
+-- | IT         | Max      | 90000  |
+-- | Sales      | Henry    | 80000  |
+-- +------------+----------+--------+
+SELECT d.Name as Department, e.Name as Employee, e.Salary FROM Employee e ,
+(SELECT e1.DepartmentId,  max(e1.Salary) as MaxSalary FROM Employee e1 GROUP BY e1.DepartmentId) as e2, Department d
+WHERE e.DepartmentId = e2.DepartmentId
+AND e.Salary = e2.MaxSalary
+AND d.Id = e.DepartmentId
