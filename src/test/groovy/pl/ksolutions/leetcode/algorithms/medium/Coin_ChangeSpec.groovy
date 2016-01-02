c10ea3433a1d32daf0b63cfef3ebcf99ccfe0f7b
Karol Kornecki <karol.kornecki@gmail.com>
2016-01-02 16:23:15 +0100

    #322 Coin Change - tests

--- src/test/groovy/pl/ksolutions/leetcode/algorithms/medium/Coin_ChangeSpec.groovy
+++ src/test/groovy/pl/ksolutions/leetcode/algorithms/medium/Coin_ChangeSpec.groovy
@@ -14,10 +14,10 @@ class Coin_ChangeSpec extends Specification {
 
         where:
         coins     | amount | expected
-//        [1, 2, 5] | 13     | 4
-//        [5, 7]    | 9      | -1
-//        []        | 7      | -1
-//        [1, 2, 5] | 0      | 0
+        [1, 2, 5] | 13     | 4
+        [5, 7]    | 9      | -1
+        []        | 7      | -1
+        [1, 2, 5] | 0      | 0
         [3, 5]    | 16     | 4
     }
 }
